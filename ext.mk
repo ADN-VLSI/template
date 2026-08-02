0145c422f7f089e8520f9ee49d4e1defb7876a7f
Foez Ahmed <119846826+foez-ahmed@users.noreply.github.com>
2026-08-02 22:26:13 +0600

    Update ext.mk

--- ext.mk
+++ ext.mk
@@ -1,7 +1,7 @@
 HAS_SUBMODULES = 1
 
-export ADN_ENDEC=$(CURDIR)
-export REPO_NAME_EXP=ADN_ENDEC
+export ADN_TEMPLATE=$(CURDIR)
+export REPO_NAME_EXP=ADN_TEMPLATE
 
 export ADN_COMMON=$(REPO_ROOT)/submodule/adn_common
 
